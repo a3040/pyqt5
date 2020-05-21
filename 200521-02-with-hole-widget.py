0d7a02422072fde79e84347df997e0d1ff913925
marca <a3040@a3040.com>
2020-05-21 20:47:24 +0900

    ..뭔가..

--- 200521-02-with-hole-widget.py
+++ 200521-02-with-hole-widget.py
@@ -1,5 +1,6 @@
 #https://stackoverflow.com/questions/61914123/is-it-possible-to-render-text-as-being-cut-out-from-a-layer-in-in-qt5
-
+#https://doc.qt.io/qt-5/qpainterpath.html
+#e
 
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QPixmap,QPainterPath, QFont, QPainter, QBrush
@@ -15,12 +16,12 @@ class Widget(QWidget):
     def makePixmap(self, text):
         background = QPixmap(600, 80)
         background.fill(Qt.red)      # Your background image
+        painter = QPainter(background)
 
         textMask = QPainterPath() 
         textMask.addRect(75, 20, 300, 40) # The white part
         textMask.addText(QPoint(90, 50), QFont("Helvetica [Cronyx]", 24), text) # the path will substract the text to the rect
 
-        painter = QPainter(background)
         painter.setRenderHints(QPainter.Antialiasing | QPainter.TextAntialiasing)
 
         painter.fillPath(textMask, QBrush(Qt.white)) # Will draw the white part with the text "cut out"

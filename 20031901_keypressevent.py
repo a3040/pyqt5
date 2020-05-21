52803920299cc14ec7c493a790c5a2ad11470504
marca <a3040@a3040.com>
2020-05-21 20:30:29 +0900

    ??

--- 20031901_keypressevent.py
+++ 20031901_keypressevent.py
@@ -1,12 +1,9 @@
 #https://stackoverflow.com/questions/60748770/why-does-my-keypressevent-not-register-or-work-pyqt5
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow)
-from PyQt5.QtCore import Qt
-
-
-class Main(QMainWindow):
-
+from PyQt5 import QtWidgets 
+ 
+class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main, self).__init__() 
 
@@ -14,10 +11,16 @@ class Main(QMainWindow):
         #if event.key() == Qt.Key_Escape:
         #if event.key() == Qt.Key_F5:
         #    print("closed")
-        print( event.key())
- 
+        print( 'in class', event.key())
+
+def ke(e):
+        print( 'in fn:', e.key())
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     M = Main()
+    M.setStyleSheet('QLabel{border:1px solid green;}')
+    QtWidgets.QLabel('키보드 누르세요.',M).resize(200, 50)
+    #M.keyPressEvent = ke
     M.show()
     sys.exit(app.exec())
